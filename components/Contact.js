9c56e1b56df1d37fe9c1b11ad4301fa457104dfb
Tomasz Wyrwa <wyrwa.t@gmail.com>
2019-09-03 13:38:50 +0200

    Change contact.js to jsx

--- components/Contact.js
+++ components/Contact.js
@@ -7,19 +7,15 @@ var Contact = React.createClass({
   
     render: function() {
       return (
-        React.createElement('div', {className: 'contactItem'},
-          React.createElement('img', {
-            className: 'contactImage',
-            src: 'https://image.flaticon.com/icons/svg/181/181549.svg'
-          }),
-          React.createElement('div', {className: 'info'},
-            React.createElement('p', {className: 'contactLabel'}, 'Name: ' + this.props.item.firstName),
-            React.createElement('p', {className: 'contactLabel'}, 'Surname: ' + this.props.item.lastName),
-            React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
-            this.props.item.email
-            )
-          )
-        )
-      )
-    },
-  });
+        <div className={'contactItem'}>
+          <img className={'contactImage'} src={'https://image.flaticon.com/icons/svg/181/181549.svg'}/>
+          <p className={'contactLabel'}>
+              Name: {this.props.item.firstName}
+          </p>
+          <a href={'mailto:' + this.props.item.email}>
+              {this.props.item.email}
+          </a>
+        </div>
+      );
+    }
+});
